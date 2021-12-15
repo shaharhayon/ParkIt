9de8ed2c0075f4aa3295d138d1d74416d7365d0d
shaharhayon <shahar.hayon9@gmail.com>
2021-12-15 16:46:33 +0200

    fix layout

--- app/src/main/java/com/parkit/ui/MapFragment.java
+++ app/src/main/java/com/parkit/ui/MapFragment.java
@@ -1,9 +1,12 @@
 package com.parkit.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
