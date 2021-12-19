54ba891bf55a9787c335e038be66aa8ba4b51e69
shaharhayon <shahar.hayon9@gmail.com>
2021-12-19 20:41:19 +0200

    permission manager

--- app/src/main/java/com/parkit/ui/MapFragment.java
+++ app/src/main/java/com/parkit/ui/MapFragment.java
@@ -1,27 +1,44 @@
 package com.parkit.ui;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.parkit.R;
 import com.parkit.ui.home.HomeFragment;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,17 +47,72 @@ import com.parkit.ui.home.HomeFragment;
  */
 public class MapFragment extends Fragment {
 
+    static final int REQUEST_LOCATION = 100;
+
+    GoogleMap gMap;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED
+                        || grantResults[1] == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(getActivity(),
+                            "Location permission denied. \nCurrent location will be disabled.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    centerLocation(getContext());
+                    Toast.makeText(getActivity(),
+                            "Location permission granted.",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+
+        }
+    }
+
+    private void centerLocation(Context context) {
+        gMap.setMyLocationEnabled(true);
+        LocationManager m = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        m.requestSingleUpdate(LocationManager.FUSED_PROVIDER, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                CameraUpdate centerLocation = CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 18);
+                gMap.moveCamera(centerLocation);
+            }
+        }, null);
+    }
+
+    private static boolean checkLocationPermissions(Context context) {
+        return ((ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                && (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED));
+    }
+
+    private void askLocationPermissions() {
+        Log.d("PERMISSIONS", "Asking device for location permissions");
+        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (checkLocationPermissions(container.getContext()) == false) {
+            askLocationPermissions();
+        }
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
-
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
-
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
+                gMap = googleMap;
+                if (checkLocationPermissions(container.getContext())) {
+                    centerLocation(container.getContext());
+                }
+
+
                 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(@NonNull LatLng latLng) {
@@ -54,6 +126,7 @@ public class MapFragment extends Fragment {
 //                        homeFragment.setFragmentResult("location",args);
 //                        googleMap.clear();
 //                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                        googleMap.clear();
                         googleMap.addMarker(markerOptions);
                     }
                 });
